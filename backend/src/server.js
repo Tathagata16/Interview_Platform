f108be4f7bf41117bf1935eb5e4e5a559e49d98b
Tathagata16 <tathagataghosh1609@gmail.com>
2026-01-14 16:47:06 +0530

    deployment preparation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
 
 const app = express();
 
+const __dirname = path.resolve();
 app.get('/',(req, res)=>{
     res.send("Hello in / ");
 })
+
+if(ENV.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(ENV.PORT,()=>{console.log("server is running at ", ENV.PORT)});
